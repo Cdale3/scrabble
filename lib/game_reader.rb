43dac1fb5ecf67afdf80fb7c4cf8c493511c74b1
Christopher Dale <epiphone2903@hotmail.com>
2017-02-17 12:05:56 -0700

    got held up trying to call the scores over

--- lib/game_reader.rb
+++ lib/game_reader.rb
@@ -1,3 +1,22 @@
+require './lib/scrabble'
 class GameReader
+  attr_reader :scrabble_word
 
+  def initialize(scrabble_word)
+    @scrabble_word = scrabble_word.upcase.split("")
+    @word_score = Scrabble.new.point_values
+  end
+
+  def score_with_multipliers(word, array)
+     score = @word_score
+    score.each do |letter, scrabble_word| puts scrabble_word
+    end
+    # p word
+    # p array
+  end
 end
+
+
+#
+# game = Scrabble.new
+# game.score_with_multipliers('hello', [1,2,1,1,1])  # => 9
--- test/game_reader_test.rb
+++ test/game_reader_test.rb
@@ -1,9 +1,15 @@
 gem 'minitest'
-require_relative '../lib/game_reader'
+require './lib/game_reader'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/scrabble'
 require 'pry'
 
 class GameReaderTest < Minitest::Test
+
+  def test_it_can_score_a_single_letter
+    game = GameReader.new('hello')
+    assert_equal 9, game.score_with_multipliers('hello', [1,2,1,1,1])
+  end
 
 end
